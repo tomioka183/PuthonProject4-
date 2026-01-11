32bebfa594ca5dcd86719a9e53a078444a7ed75c
Your Name <you@example.com>
2026-01-11 19:37:01 +0300

    Final fix: Add 9.1 requirements (Black, Isort, Mypy configs, main.py, poetry.lock)

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.widget import mask_account_card, get_date
+
+if __name__ == "__main__":
+    print(mask_account_card("Visa Platinum 7000792289606361"))
+    print(get_date("2024-03-11T02:26:18.671407"))
+
